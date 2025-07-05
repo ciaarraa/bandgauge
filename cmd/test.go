1668cdf737c8999e7dc9d92153775c6266e42aab
CiaraTully <ciara.tully@runa.io>
2025-07-05 20:41:41 +0100

    Add test command
    Add a test command with hard coded IP address and port. The test command
    runs an iperf3 test and prints the raw output to the console as json

---
+++ cmd/test.go
@@ -0,0 +1,32 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+)
+
+// testCmd represents the test command
+var testCmd = &cobra.Command{
+	Use:   "test",
+	Short: "Run a bandwidth test",
+	Long: `Run a bandwith test using iperf3. You must have an iperf3 server running on the remote host and
+	ip address configured.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		run := exec.Command("iperf3", "-c", "192.168.1.118", "-J")
+		output, err := run.CombinedOutput()
+		if err != nil {
+			fmt.Println("Error running iperf3:", err)
+			return
+		}
+		fmt.Println("Output from iperf3:", string(output))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(testCmd)
+}
